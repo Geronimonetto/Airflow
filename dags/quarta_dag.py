d8015ea55876f4e3474c43cbecc66c674871187d
geronimo morais <geronimomorais1617@gmail.com>
2024-01-22 14:59:00 -0300

    Airflow - Criando Quarta dag

---
+++ dags/quarta_dag.py
@@ -0,0 +1,14 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+with DAG('quarta_dag', description = 'Quarta DAG',
+            schedule_interval = None, start_date=datetime(2024, 1, 22), 
+            catchup=False) as dag:
+
+    task1 = BashOperator(task_id='task_extract1', bash_command='sleep 5')
+    task2 = BashOperator(task_id='task_extract2', bash_command='sleep 5')
+    task3 = BashOperator(task_id='task_extract3', bash_command='sleep 5')
+
+
+    task1.set_upstream(task2)  #Proxima task a ser executada
+    task2.set_upstream(task3)  #Proxima task a ser executada
