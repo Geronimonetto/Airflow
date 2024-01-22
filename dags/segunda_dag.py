28509441e4b61b9f15d89364341a7022cea3d50b
geronimo morais <geronimomorais1617@gmail.com>
2024-01-22 14:59:54 -0300

    Airflow - Modificando segunda dag

--- dags/segunda_dag.py
+++ dags/segunda_dag.py
@@ -3,11 +3,11 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 
 
-dag = DAG('extract_data', description='Its dag extract data from S3', schedule_interval=None,
-          start_date=datetime(2024,1,16), catchup=False)
+dag = DAG('extract_data_two', description='Its dag extract data from S3, and send for db', schedule_interval=None,
+          start_date=datetime(2024, 1, 22), catchup=False)
 
 task1 = BashOperator(task_id='task_extract1', bash_command='sleep 5', dag=dag)
 task2 = BashOperator(task_id='task_extract2', bash_command='sleep 5', dag=dag)
 task3 = BashOperator(task_id='task_extract3', bash_command='sleep 5', dag=dag)
 
-task1 >> task2
+task1 >> [task2, task3]
