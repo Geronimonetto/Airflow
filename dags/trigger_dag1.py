769c79c1867fe75856204a090dedd0bec5d4808f
geronimo morais <geronimomorais1617@gmail.com>
2024-01-23 18:06:24 -0300

    Airflow - Criando exemplo de trigger 1

---
+++ dags/trigger_dag1.py
@@ -0,0 +1,12 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, date
+
+
+dag = DAG('exemplo_trigger', description= 'Its dag is of very comprehension!', schedule_interval = None, start_date = datetime(2024, 1, 22), catchup=False)
+
+task1 = BashOperator(task_id='extract_task1', bash_command='sleep 5', dag=dag)
+task2 = BashOperator(task_id='extract_task2', bash_command='sleep 5', dag=dag)
+task3 = BashOperator(task_id='extract_task3', bash_command='sleep 5', dag=dag, trigger_rule='one_failed')
+
+[task1, task2] >> task3 
