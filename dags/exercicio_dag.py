41b6c10fe6dc578672a53ec1cb03e1a5aefab0aa
geronimo morais <geronimomorais1617@gmail.com>
2024-02-09 17:29:29 -0300

    Airflow - Praticando exercícios de dags

--- dags/exercicio_dag.py
+++ dags/exercicio_dag.py
@@ -1,26 +1,40 @@
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
-from datetime import datetime
+from datetime import datetime, date
+from airflow.operators.python_operator import BranchPythonOperator
+from random import randint
+
+dag = DAG('Dag_example', description='Está dag é um exemplo de branch', schedule_interval=None,
+          start_date=datetime(date.today().year, date.today().month, date.today().day), catchup=False)
 
 
-dag = DAG('task_duummy_exercicio', description='This is task dummy, where we create a conection with parallel tasks', schedule_interval='@hourly',
-          start_date=datetime(2024, 2, 1), catchup=False)
+def gera_numero() -> int:
+    return randint(1, 1000)
 
-task1 = BashOperator(task_id='dummy_task_one', bash_command='sleep 2', dag=dag)
-task2 = BashOperator(task_id='dummy_task_two', bash_command='sleep 2', dag=dag)
-task3 = BashOperator(task_id='dummy_task_three',
-                     bash_command='sleep 2', dag=dag)
-task_connection = DummyOperator(task_id='Task_connection', dag=dag)
-task_connection2 = DummyOperator(task_id='Task_connection2', dag=dag)
-task4 = BashOperator(task_id='dummy_task_four',
-                     bash_command='sleep 2', dag=dag)
-task5 = BashOperator(task_id='dummy_task_five',
-                     bash_command='sleep 2', dag=dag)
-task6 = BashOperator(task_id='dummy_task_six',
-                     bash_command='sleep 2', dag=dag)
-task7 = BashOperator(task_id='dummy_task_seven',
-                     bash_command='sleep 2', dag=dag)
 
-[task1, task2]  >> task_connection >> [task3, task4] >> task_connection2
-task_connection2 >> [task5, task6, task7]
+task_gera_numero = PythonOperator(
+    task_id='task_gerador', python_callable=gera_numero, dag=dag)
+
+
+def recebe_numero(**args):
+    numero = args['task_instance'].xcom_pull(task_ids='task_gerador')
+
+    if numero % 2 == 0:
+        return 'par_tasks'  # Deve ser a string com o nome da task_id
+    else:
+        return 'impar_tasks'  # O mesmo se repete aqui, deve-se por o nome do task_id
+
+
+branch_task = BranchPythonOperator(
+    task_id='recebe_numero_task', python_callable=recebe_numero, provide_context=True, dag=dag)
+
+par_task = BashOperator(task_id='par_tasks',
+                        bash_command='echo "Par"', dag=dag)
+impar_task = BashOperator(task_id='impar_tasks',
+                          bash_command='echo "impar"', dag=dag)
+
+
+task_gera_numero >> branch_task
+branch_task >> par_task
+branch_task >> impar_task
