226f11093d5e8659db00b6668f161ca9b8878d2e
geronimo morais <geronimomorais1617@gmail.com>
2024-02-15 20:39:26 -0300

    Airflow - Criando plugins para utilização nas dags

---
+++ plugins/big_data_operator.py
@@ -0,0 +1,25 @@
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+import pandas as pd
+
+
+class BigDataOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, path_to_csv_file: str, path_to_save_file: str,
+                 separator: str = ';', file_type: str = 'parquet', *args,
+                 **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.path_to_csv_file: str = path_to_csv_file
+        self.path_to_save_file: str = path_to_save_file
+        self.separator: str = separator
+        self.file_type: str = file_type
+
+    def execute(self, context: str) -> None:
+        df = pd.read_csv(self.path_to_csv_file, sep=self.separator)
+        if self.file_type == 'parquet':
+            df.to_parquet(self.path_to_save_file)
+        elif self.file_type == 'json':
+            df.to_json(self.path_to_save_file)
+        else:
+            raise ValueError("O Tipo é Inválido!!")
